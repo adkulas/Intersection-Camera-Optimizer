36183fe4045e4b0604de9bba6814c188dfc3fdfe
Adam Kulas <adam.kulas1@gmail.com>
2018-12-01 11:20:57 -0500

    implemented approx_vc_1

--- approx_vc.cpp
+++ approx_vc.cpp
@@ -2,6 +2,7 @@
 #include <vector>
 #include <list>
 #include <climits>
+#include <algorithm>
 #include "approx_vc.hpp"
 
 bool check_valid_input(int v, std::vector< std::pair<int,int> > edges) {
@@ -24,12 +25,26 @@ bool check_valid_input(int v, std::vector< std::pair<int,int> > edges) {
 std::vector< std::vector<int> > create_graph_adj_list(int v, std::vector< std::pair<int,int> > edges) {
     std::vector< std::vector<int> > graph(v);
     for ( auto& e : edges) {
-            std::cout << e.first << " " << e.second << std::endl;
             graph[e.first].push_back(e.second);
             graph[e.second].push_back(e.first);
         }
     return graph;
+}
+
+
+std::vector< std::vector<int> > create_graph_adj_matrix(int v, std::vector< std::pair<int,int> > edges) {
     
+    // initialize v x v matrix
+    std::vector< std::vector<int> > graph(v);
+    for ( int i = 0 ; i < v ; i++ )
+        graph[i].resize(v);
+
+    // add edges between vertices
+    for ( auto& e : edges) {
+            graph[e.first][e.second] = 1;
+            graph[e.second][e.first] = 1;
+        }
+    return graph;
 }
 
 void approx_vc_1(int v, std::vector< std::pair<int,int> > edges) {
@@ -37,9 +52,59 @@ void approx_vc_1(int v, std::vector< std::pair<int,int> > edges) {
         return;
     }
 
-    std::vector< std::vector<int> > graph;
-    graph = create_graph_adj_list(v, edges);
-    std::cout << "APPROX-VC-1: " << std::endl;
+    std::vector<int> v_cover;
+    std::vector< std::vector<int> > graph = create_graph_adj_matrix(v, edges);
+
+    for (auto r : graph) {
+        for (auto c : r) {
+            std::cout << c << " ";
+        }
+        std::cout << std::endl;
+    }
+    while(true) {
+        // pick highest degree vertex
+        int vertex = -1;
+        int max_degree = 0;
+        for (int r=0; r < graph.size(); r++) {
+            int v_deg = 0;
+            for (auto& n : graph[r])
+                v_deg += n;
+             
+            if (v_deg > max_degree) {
+                vertex = r;
+                max_degree = v_deg;
+            }
+        }
+        
+        // break if no edges remain (ever vertex has degree of 0)
+        if (vertex < 0) {
+            break;
+        }
+
+        // remove edges incident to vertex
+        for (int i=0; i < graph.size(); i++) { // reset column to 0
+            graph[i][vertex] = 0;
+            graph[vertex][i] = 0;
+        }
+
+        // add vertex to cover
+        v_cover.push_back(vertex);
+        std::clog << "Adding vertex: " << vertex << std::endl;
+        std::clog << "Current Graph:" << std::endl;
+        for (auto& r : graph) {
+            for (auto& c : r) {
+                std::clog << c << " ";
+            }
+            std::clog << std::endl;
+        }
+    }
+    std::sort(v_cover.begin(), v_cover.end());
+
+    std::cout << "APPROX-VC-1: ";
+    for (auto& v : v_cover) {
+        std::cout << v << " ";
+    } 
+    std::cout << std::endl;
 
     return;
 }
--- approx_vc.hpp
+++ approx_vc.hpp
@@ -5,6 +5,7 @@
 
 // helper functions
 std::vector< std::vector<int> > create_graph_adj_list(int v, std::vector< std::pair<int,int> > edges);
+std::vector< std::vector<int> > create_graph_adj_matrix(int v, std::vector< std::pair<int,int> > edges);
 bool check_valid_input(int v, std::vector< std::pair<int,int> > edges);
 
 // approximation vertex cover algorithm 1
