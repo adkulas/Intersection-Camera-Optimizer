b7837aea0a84e542b0cacaf14fd9961d4665bf14
Adam Kulas <adam.kulas1@gmail.com>
2018-12-03 14:50:00 -0500

    working prototype of producer consumer threads

--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@
 #include <vector>
 #include <list>
 #include <iostream>
+#include <unistd.h>
 #include <pthread.h>
 #include "cnf-sat-vc.hpp"
 #include "approx_vc.hpp"
@@ -101,14 +102,93 @@ struct job {
 };
 
 pthread_mutex_t job_queue_mutex = PTHREAD_MUTEX_INITIALIZER;
-std::list<job> job_queue;
+std::list<job*> job_queue;  // stores a list of pointers to jobs (structs containing information)
+std::list<std::string> result_queue;
+
+void* test_producer(void* args) {
+    std::cout << "ENTERED PRODUCER FUNC" << std::endl;
+    int count = 0;
+    while(true) {
+        // create a job
+        struct job* new_job = new job;
+        new_job->vertices = count;
+        new_job->edges = "test";
+        
+        // add to queue
+        std::cout << "Adding job: " << count << std::endl;
+        job_queue.push_back(new_job);
+
+        count ++;
+        usleep(600000);
+        // output from result queue
+        if (!result_queue.empty()) {
+            std::string output = result_queue.front();
+            result_queue.pop_front();
+            std::cout << "Work Complete - Result: " << output << std::endl;
+        }
+
+    }
+
+    return NULL;
+}
+
+void* test_consumer(void* args) {
+    std::cout << "ENTERED CONSUMER FUNC" << std::endl;
+    while(true) {
+        if (!job_queue.empty()) {
+            // get job from queue
+            struct job* tmp_job = new job;
+            tmp_job = job_queue.front(); 
+            job_queue.pop_front();
+
+            // do work on job
+            std::cout << "Working on job :" << tmp_job->vertices << std::endl;
+            
+
+            // put result into queue
+            std::string result = "COMPLETE " + std::to_string(tmp_job->vertices);
+            std::cout << "Pushing result for: " << tmp_job->vertices << std::endl;
+            result_queue.push_back(result);
+            
+            // cleanup
+            delete tmp_job;
+        }
+        
+        sleep(1);
+    }
+
+    return NULL;
+}
+
 
 int main() {
     
-    pthread_t thread_id;
-    pthread_create (&thread_id, NULL, &IO_handler, NULL);
-    // pthread_create (&thread2_id, NULL, &dequeue_job, NULL);
+    // pthread_t IO_thread;
+    // pthread_t cnf_sat_thread;
+    // pthread_t approx_vc1_thread;
+    // pthread_t approx_vc2_thread;
+
+    // pthread_create (&IO_thread, NULL, &IO_handler, NULL);
+    // pthread_create (&cnf_sat_thread, NULL, &IO_handler, NULL);
+    // pthread_create (&approx_vc1_thread, NULL, &IO_handler, NULL);
+    // pthread_create (&approx_vc2_thread, NULL, &IO_handler, NULL);
+
+    // std::cout << "waiting..." << std::endl;
+    // pthread_join (IO_thread, NULL);
+    // pthread_join (cnf_sat_thread, NULL);
+    // pthread_join (approx_vc1_thread, NULL);
+    // pthread_join (approx_vc2_thread, NULL);
+
+
+    pthread_t thd_1;
+    pthread_t thd_2;
+
+    pthread_create (&thd_1, NULL, &test_producer, NULL);
+    pthread_create (&thd_2, NULL, &test_consumer, NULL);
+
     std::cout << "waiting..." << std::endl;
-    pthread_join (thread_id, NULL);
+    pthread_join (thd_1, NULL);
+    pthread_join (thd_2, NULL);
+
     return 0;
 }
